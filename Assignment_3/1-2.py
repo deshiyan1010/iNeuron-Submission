839b9a5ee9b990f69434f1ca93cb0b7d35e46d1b
deshiyan1010 <56188225+deshiyan1010@users.noreply.github.com>
2020-05-15 18:47:35 +0530

    Create 1-2.py

---
+++ Assignment_3/1-2.py
@@ -0,0 +1,8 @@
+subjects=["Americans ","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            print(str(subjects[i])+" "+str(verbs[j])+" "+str(objects[k]))
